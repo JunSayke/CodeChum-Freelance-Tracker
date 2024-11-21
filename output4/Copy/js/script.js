ffda1bfc1ccccfa8d2d21d3042777cb7118f36d1
Antonio Mancera Ubaldo Jr <142708326+JunSayke@users.noreply.github.com>
2024-11-22 00:39:37 +0800

    [Hunyo] Mobile Responsive WIP

--- output4/Copy/js/script.js
+++ output4/Copy/js/script.js
@@ -1,38 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-	// Get all parallax elements
-	const parallaxContainer = document.querySelector(
-		"#first-section.parallax-container"
-	)
-	const snowBallsPattern = parallaxContainer.querySelector(
-		".snow-pattern.snow-balls"
-	)
-	const snowFlakesPattern = parallaxContainer.querySelector(
-		".snow-pattern.snow-flakes"
-	)
-	const treesPattern = parallaxContainer.querySelector(".trees-pattern")
-	const reindeerLeftPattern = parallaxContainer.querySelector(
-		".reindeer-pattern.reindeer-left"
-	)
-	const reindeerRightPattern = parallaxContainer.querySelector(
-		".reindeer-pattern.reindeer-right"
-	)
-
-	/*
-	 * Parallax speed rate
-	 * The higher the number, the slower the element will move
-	 * The lower the number, the faster the element will move
-	 * All uses pixels as unit except for reindeer which uses percentage
-	 */
+	// Parallax speed rate
 	const parallaxSpeed = {
 		snowBalls: 2,
 		snowFlakes: 3,
 		trees: 5,
-		reindeer: 0.1,
+		reindeer: 1,
 	}
 
-	/*
-	 * Update parallax elements positions based on scroll position
-	 */
+	// Update parallax elements positions based on scroll position
 	function updateParallax() {
 		// Get scroll position
 		const scrollPosition = window.scrollY
@@ -40,27 +15,47 @@ document.addEventListener("DOMContentLoaded", function () {
 		const windowHeight = window.innerHeight
 		const windowWidth = window.innerWidth
 
-		// Calculate new positions
-		const snowBallsY = (scrollPosition * parallaxSpeed.snowBalls) % windowHeight
-		const snowFlakesY =
-			(scrollPosition * parallaxSpeed.snowFlakes) % windowHeight
-		const treesX = (scrollPosition * parallaxSpeed.trees) % windowWidth
-		const reindeerLeftX =
-			(15 + scrollPosition * -parallaxSpeed.reindeer) % windowWidth
-		const reindeerRightX =
-			(85 + scrollPosition * parallaxSpeed.reindeer) % windowWidth
-		const reindeerY =
-			(50 - scrollPosition * parallaxSpeed.reindeer) % windowHeight
+		// Select all elements with data-parallax attribute
+		const parallaxElements = document.querySelectorAll("[data-parallax]")
 
-		// Set new positions
-		snowBallsPattern.style.backgroundPosition = `center ${snowBallsY}px`
-		snowFlakesPattern.style.backgroundPosition = `center ${snowFlakesY}px`
-		treesPattern.style.backgroundPosition = `${treesX}px bottom`
-		reindeerLeftPattern.style.backgroundPosition = `${reindeerLeftX}% ${reindeerY}%`
-		reindeerRightPattern.style.backgroundPosition = `${reindeerRightX}% ${reindeerY}%`
+		parallaxElements.forEach((element) => {
+			const parallaxType = element.dataset.parallax
+
+			let positionX, positionY
+			switch (parallaxType) {
+				case "snow-balls":
+					positionY = (scrollPosition * parallaxSpeed.snowBalls) % windowHeight
+					element.style.backgroundPosition = `center ${positionY}px`
+					break
+				case "snow-flakes":
+					positionY = (scrollPosition * parallaxSpeed.snowFlakes) % windowHeight
+					element.style.backgroundPosition = `center ${positionY}px`
+					break
+				case "trees":
+					positionX = (scrollPosition * parallaxSpeed.trees) % windowWidth
+					element.style.backgroundPosition = `${positionX}px bottom`
+					break
+				case "reindeer-left":
+					positionX = (scrollPosition * -parallaxSpeed.reindeer) % windowWidth
+					positionY = -(scrollPosition * parallaxSpeed.reindeer) % windowHeight
+					element.style.transform = `translate(${positionX}px, ${positionY}px) translateY(-50%)`
+					break
+				case "reindeer-right":
+					positionX = (scrollPosition * parallaxSpeed.reindeer) % windowWidth
+					positionY = -(scrollPosition * parallaxSpeed.reindeer) % windowHeight
+					element.style.transform = `translate(${positionX}px, ${positionY}px) translateY(-50%)`
+					break
+			}
+		})
+	}
+
+	// Use requestAnimationFrame for smoother animations
+	function onScroll() {
+		requestAnimationFrame(updateParallax)
 	}
 
 	// Attach scroll event listener
-	window.addEventListener("scroll", updateParallax)
-	updateParallax() // Initial call to set positions on load
+	window.addEventListener("scroll", onScroll)
+	// Initial call to set positions on load
+	updateParallax()
 })
